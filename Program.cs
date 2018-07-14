1d8aa2b14eff9cadfc6ebb83503f8498ba5a4fe0
Katelynn Campbell <klyncamp23@gmail.com>
2018-07-13 20:05:46 -0400

    Razor Pages

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace RazorPagesMovie
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); //
     }
 }
